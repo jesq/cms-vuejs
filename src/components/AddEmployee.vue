88a3ee08e953bb310469c639944f12fd2d3960f1
Cosmin Jescu <57216949+jesq@users.noreply.github.com>
2021-08-18 23:52:16 +0300

    Added: Add Employee form

--- src/components/AddEmployee.vue
+++ src/components/AddEmployee.vue
@@ -1,6 +1,46 @@
 <template>
     <div id="add-meployee">
-        <h3>Add Employee</h3>
+        <h3 class="text-center m-5">Add Employee</h3>
+        <form id="employee-form">
+            <div class="form-group mt-5">
+                <label for="firstName">
+                    First Name
+                </label>
+                <input type="text" id="firstName" class="form-control text-white bg-light">
+            </div>
+            <div class="form-group mt-3">
+                <label for="lastName">
+                    Last Name
+                </label>
+                <input type="text" id="lastName" class="form-control text-white bg-light">
+            </div>
+            <div class="form-group mt-3">
+                <label for="gender">
+                    Gender
+                </label>
+                <select id="gender" class="form-select text-white bg-light">
+                    <option selected>Select one</option>
+                    <option value="Male">Male</option>
+                    <option value="Female">Female</option>
+                    <option value="Other">Other</option>
+                </select>
+            </div>
+            <div class="form-group mt-3">
+                <label for="birthday">
+                    Birthday
+                </label>
+                <br>
+                <input class="bg-light text-white" type="date" id="birthday" name="birthday">
+            </div>
+            <div class="form-group mt-3">
+                <label for="emailAddress">
+                    Email Address
+                </label>
+                <input type="text" id="emailAddress" class="form-control text-white bg-light">
+            </div>
+            <input type="submit" value="Add Employee"
+            class="btn btn-primary col-12 mt-5" id="submitBtn">
+        </form>
     </div>
 </template>
 
