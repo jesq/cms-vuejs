d85fb50ff1defc47d92e3251097a3f5541581d5f
Cosmin Jescu <57216949+jesq@users.noreply.github.com>
2021-08-18 20:41:21 +0300

    Added navbar

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,17 @@
 <template>
   <div id="app">
+    <Navbar />
     <router-view/>
   </div>
 </template>
+
+<script>
+import Navbar from './components/Navbar.vue'
+export default {
+  name: 'app',
+  components: {
+    Navbar
+  }
+}
+</script>
 
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,5 +1,15 @@
 <template>
-    <nav >
-
-    </nav>
+  <nav>
+      <div class="container">
+        <router-link to="/" class="logo">
+          <h1 class="display-4 text-center">
+            <i class="fas fa-cogs text-primary"></i>
+            Content Management
+            <span class="text-primary">
+              System
+            </span>
+          </h1>
+        </router-link>
+      </div>
+  </nav>
 </template>
