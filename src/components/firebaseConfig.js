470b01b221f636378ecd139a0d146f9c373068e0
Cosmin Jescu <57216949+jesq@users.noreply.github.com>
2021-08-18 19:46:13 +0300

    Configured firebase

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyCYh0jg9MP7tTGxL6sVsN4nmrLPYAy6jN0",
+    authDomain: "cms-vuejs-bca67.firebaseapp.com",
+    projectId: "cms-vuejs-bca67",
+    storageBucket: "cms-vuejs-bca67.appspot.com",
+    messagingSenderId: "941877411380",
+    appId: "1:941877411380:web:004496320102e726a75461",
+    measurementId: "G-C1G4GBEHJ2"
+}
