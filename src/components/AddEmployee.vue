84356b0cee63490279c69c58bbef8f21404f611c
Cosmin Jescu <57216949+jesq@users.noreply.github.com>
2021-08-19 15:37:30 +0300

    Added: "back button" for "add employee form"

--- src/components/AddEmployee.vue
+++ src/components/AddEmployee.vue
@@ -1,24 +1,24 @@
 <template>
     <div id="add-meployee">
         <h3 class="text-center m-5">Add Employee</h3>
-        <form id="employee-form">
+        <form @submit.prevent="addEmployee" id="employee-form">
             <div class="form-group mt-5">
                 <label for="firstName">
                     First Name
                 </label>
-                <input type="text" id="firstName" class="form-control text-white bg-light">
+                <input type="text" id="firstName" v-model="firstName" required class="form-control text-white bg-light">
             </div>
             <div class="form-group mt-3">
                 <label for="lastName">
                     Last Name
                 </label>
-                <input type="text" id="lastName" class="form-control text-white bg-light">
+                <input type="text" id="lastName" v-model="lastName" required class="form-control text-white bg-light">
             </div>
             <div class="form-group mt-3">
                 <label for="gender">
                     Gender
                 </label>
-                <select id="gender" class="form-select text-white bg-light">
+                <select id="gender" v-model="gender" required class="form-select text-white bg-light">
                     <option selected>Select one</option>
                     <option value="Male">Male</option>
                     <option value="Female">Female</option>
@@ -30,16 +30,19 @@
                     Birthday
                 </label>
                 <br>
-                <input class="bg-light text-white" type="date" id="birthday" name="birthday">
+                <input class="bg-light text-white" type="date" v-model="birthday" required id="birthday" name="birthday">
             </div>
             <div class="form-group mt-3">
                 <label for="emailAddress">
                     Email Address
                 </label>
-                <input type="text" id="emailAddress" class="form-control text-white bg-light">
+                <input type="text" id="emailAddress" v-model="emailAddress" required class="form-control text-white bg-light">
             </div>
             <input type="submit" value="Add Employee"
-            class="btn btn-primary col-12 mt-5" id="submitBtn">
+            class="btn btn-primary col-12 mt-3" id="submitBtn">
+            <router-link to="/" class="btn btn-secondary col-12 mt-2">
+                Back
+            </router-link>
         </form>
     </div>
 </template>
